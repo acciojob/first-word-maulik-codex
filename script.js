3abda5f766e0b6c1210b06771574d44fea2e6402
Maulik Chhansiya <maulikchhansiya@gmail.com>
2025-06-17 23:06:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
-  // your code here
+  // Trim leading spaces first to handle strings like " Hello World"
+  s = s.trimStart();
+
+  // Find index of first space
+  const spaceIndex = s.indexOf(' ');
+
+  // If no space found, return the whole string
+  if (spaceIndex === -1) {
+    return s;
+  }
+
+  // Otherwise return substring up to first space
+  return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
